89d11ef41f88fc55fc629c164d360a1e7ba1204f
mlcJS <marcelacorrea1979@gmail.com>
2019-05-07 19:00:53 -0300

    clase 8

--- Clase 07/Profe/En clase/js/index.js
+++ Clase 07/Profe/En clase/js/index.js
@@ -1,89 +1,89 @@
-// # 7 SesionStorage y LocalStorage [Modulo B]
+//# 7 SesionStorage y LocalStorage [Modulo B]
 
-// - 7.1	¿Qué es el sesionStorage y el localStorage?
-//    - `setItem(key, value)`
-//    - `getItem(key)`
-//    - `removeItem(key)`
-// - 7.2	¿Qué es un JSON?
-// - 7.3	`JSON.stringify()` y `JSON.parse()`
+//7.1	¿Qué es el sesionStorage y el localStorage?
+//`setItem(key, value)`
+//`getItem(key)`
+//`removeItem(key)`
+//7.2	¿Qué es un JSON?
+//7.3	`JSON.stringify()` y `JSON.parse()`
 
-// console.log('Json vs Freddy')
+console.log('Json vs Freddy')
 
-// var kitty = {
-//   name: 'Princess Caroline',
-//   age: 1.5,
-//   colors: [ 'white', 'grey', 'coffee' ],
-//   male: false,
-//   owner: {
-//     firstName: 'Camila',
-//     lastName: 'Rubin'
-//   }
-// }
+var kitty = {
+  name: 'Princess Caroline',
+  age: 1.5,
+  colors: ['white', 'grey', 'coffee'],
+  male: false,
+  owner: {
+    firstName: 'Camila',
+    lastName: 'Rubin'
+  }
+}
 
-// // console.log(kitty.owner.firstName)
+console.log(kitty.owner.firstName)
 
-// console.log(kitty.name)
+console.log(kitty.name)
 
-// var stingyKitty = JSON.stringify(kitty)
+var stingyKitty = JSON.stringify(kitty)
 
-// // console.log(stingyKitty)
+console.log(stingyKitty)
 
-// console.log(stingyKitty)
+console.log(stingyKitty)
 
-// var parsedKitty = JSON.parse(stingyKitty)
+var parsedKitty = JSON.parse(stingyKitty)
 
-// console.log(parsedKitty)
+console.log(parsedKitty)
 
-// var localFavoriteDessert = localStorage.getItem('favoriteDessert')
+var localFavoriteDessert = localStorage.getItem('favoriteDessert')
 
-// var favoriteDessert
+var favoriteDessert
 
-// if (!localFavoriteDessert) {
-//   favoriteDessert = prompt('Ingresá tu postre preferido')
+if (!localFavoriteDessert) {
+  favoriteDessert = prompt('Ingresá tu postre preferido')
 
-//   localStorage.setItem('favoriteDessert', favoriteDessert)
-// } else {
-//   favoriteDessert = localFavoriteDessert
-// }
+  localStorage.setItem('favoriteDessert', favoriteDessert)
+} else {
+  favoriteDessert = localFavoriteDessert
+}
 
-// var message = 'Tu postre preferido es ' + favoriteDessert
+var message = 'Tu postre preferido es ' + favoriteDessert
 
-// var kitty = {
-//   name: 'Princess Caroline',
-//   age: 1.5,
-//   colors: [ 'white', 'grey', 'coffee' ],
-//   male: false,
-//   owner: {
-//     firstName: 'Camila',
-//     lastName: 'Rubin'
-//   }
-// }
+var kitty = {
+  name: 'Princess Caroline',
+  age: 1.5,
+  colors: ['white', 'grey', 'coffee'],
+  male: false,
+  owner: {
+    firstName: 'Camila',
+    lastName: 'Rubin'
+  }
+}
 
-// Levanto del LS
+//Levanto del LS//
 
 var localKitty = localStorage.getItem('kittyHouse')
 
-// Convierto a JS
+//Convierto a JS//
 
 var parsedKitty = JSON.parse(localKitty)
 
-// Changes Add color
+//Changes Add color//
 
-// var newColor = prompt('Ingresé un color')
+var newColor = prompt('Ingresé un color')
 
-// parsedKitty.colors.push(newColor)
+parsedKitty.colors.push(newColor)
 
-// Changes Delete color
+//Changes Delete color//
 
-// var newColor = prompt('Ingresé un color')
+var newColor = prompt('Ingresé un color')
 
-// var colorIndex = parsedKitty.colors.indexOf(newColor)
+var colorIndex = parsedKitty.colors.indexOf(newColor)
 
-// if (colorIndex !== -1) {
-//   parsedKitty.colors.splice(colorIndex, 1)
-// }
+if (colorIndex !== -1) {
+  parsedKitty.colors.splice(colorIndex, 1)
+}
 
-// Change Modify color
+//Change Modify color//
 
 var newColor = prompt('Ingresé un color a eliminar')
 
@@ -97,10 +97,10 @@ if (colorIndex !== -1) {
   parsedKitty.colors[colorIndex] = newColor2
 }
 
-// Convierto a JSON
+//Convierto a JSON//
 
 var stringyKitty = JSON.stringify(parsedKitty)
 
-// Piso el valor en el LS
+//Piso el valor en el LS//
 
 localStorage.setItem('kittyHouse', stringyKitty)
